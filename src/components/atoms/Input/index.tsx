a3a26cef79a2d12bb45b80f7e31d8e7ca2e63176
hyejin30 <tyui3185@gmail.com>
2023-01-21 01:49:37 +0900

    Delete : InputValueNum

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -2,7 +2,6 @@ import Layout from './components/Layout';
 import Label from 'components/atoms/Label';
 import Contents from './components/Contents';
 import Value from './components/Value';
-import ValueNum from './components/ValueNum';
 import Right from './components/Right';
 import SubMessage from './components/SubMessage';
 
@@ -10,7 +9,6 @@ const Input = Object.assign(Layout, {
   Label,
   Contents,
   Value,
-  ValueNum,
   Right,
   SubMessage,
 });
