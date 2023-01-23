b99cf5bd7ac3b84bdb9c2f307de08350d53a8c81
hyejin30 <tyui3185@gmail.com>
2023-01-23 12:55:23 +0900

    Add : ReactQueryDevTools

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Allocation from 'pages/Allocation';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import GlobalStyle from 'styles/globalStyle';
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen />
+      <GlobalStyle />
       <BrowserRouter>
         <Routes>
           <Route element={<Allocation />} path="/" />
