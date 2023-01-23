835c2f168e8047d54e9c298e4f023663261a8f01
hyejin30 <tyui3185@gmail.com>
2023-01-23 19:51:31 +0900

    Add : 날짜 직접 입력 기능 추가 (onChange, onBlur)

--- src/components/atoms/Input/Input/components/Value.tsx
+++ src/components/atoms/Input/Input/components/Value.tsx
@@ -7,7 +7,6 @@ interface IValueProps extends HTMLAttributes<HTMLInputElement> {
   maxLength?: number;
   max?: string;
   min?: string;
-  pattern?: string;
   placeholder?: string;
   readOnly?: boolean;
   type?: 'text' | 'number' | 'email' | 'password';
--- src/components/molecule/Select/DateSelect/components/Input.tsx
+++ src/components/molecule/Select/DateSelect/components/Input.tsx
@@ -1,44 +1,48 @@
-import { MouseEvent } from 'react';
+import { MouseEvent, ChangeEvent, FocusEvent } from 'react';
 import styled from 'styled-components';
 
 import { Input } from 'components/atoms';
 
-import { date } from 'utils/regex';
+// import { dateRegex } from 'utils/regex';
 import { theme } from 'styles';
 
 interface IDateSelectInputProps {
   title: string;
   value: string;
+  onBlur: (e: FocusEvent<HTMLInputElement>) => void;
   onClick: (e: MouseEvent<HTMLDivElement>) => void;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 function DateSelectInput(props: IDateSelectInputProps) {
-  const { onClick, ...restProps } = props;
+  const { title, onClick, onChange, ...restProps } = props;
 
   return (
-    <Input>
-      <StyledInputValue readOnly pattern={date} {...restProps} />
+    <StyledInput>
+      <Input.Value title={title} onChange={onChange} {...restProps} />
       <Input.Right>
-        <CalendarIcon title={restProps.title} onClick={onClick}>
+        <CalendarIcon title={title} onClick={onClick}>
           <img alt="calendar" src="/images/ic-calendar-orange.svg" />
         </CalendarIcon>
       </Input.Right>
-    </Input>
+    </StyledInput>
   );
 }
 
 export default DateSelectInput;
 
-const StyledInputValue = styled(Input.Value)`
-  border: 1px solid ${theme.border.lightGray};
-  border-radius: 5px;
+const StyledInput = styled(Input)`
+  input {
+    border: 1px solid ${theme.border.lightGray};
+    border-radius: 5px;
 
-  :hover {
-    border: 1px solid ${theme.white};
-  }
+    :hover {
+      border: 1px solid ${theme.white};
+    }
 
-  :focus {
-    border: 1px solid ${theme.orange};
+    :focus {
+      border: 1px solid ${theme.orange};
+    }
   }
 `;
 
